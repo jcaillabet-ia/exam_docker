30aa18780397d86109627b42f92548edc8951ac6
Ubuntu <ubuntu@ip-172-31-47-155.eu-west-1.compute.internal>
2026-07-02 10:01:49 +0000

    Relecture

--- Test_3/main.py
+++ Test_3/main.py
@@ -66,7 +66,91 @@ output = '''
 request done at "/v1/sentiment"
 | username="alice"
 | password="wonderland"
+| sentence="that sucks"
+
+Expected result < 0; 
+actual result = {score}
+
+==>  {test_status}
+'''
+
+# résultat de la requête
+score = r.json()['score']
+
+# affichage des résultats
+if score < 0:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+output = output.format(score=score, test_status=test_status)
+print(output)
+
+# impression dans un fichier
+if os.environ.get('LOG') == '1':
+    with open('api_test.log', 'a') as file:
+        file.write(output)
+
+# requête 3
+r = requests.get(
+    url='http://{address}:{port}/v2/sentiment'.format(address=api_address, port=api_port),
+    params= {
+        'username': 'alice',
+        'password': 'wonderland',
+        'sentence': 'life is beautiful'
+    }
+)
+
+output = '''
+============================
+    Content test
+============================
+
+request done at "/v2/sentiment"
+| username="alice"
+| password="wonderland"
 | sentence="life is beautiful"
+
+Expected result > 0; 
+actual result = {score}
+
+==>  {test_status}
+'''
+
+# résultat de la requête
+score = r.json()['score']
+
+# affichage des résultats
+if score > 0:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+output = output.format(score=score, test_status=test_status)
+print(output)
+
+# impression dans un fichier
+if os.environ.get('LOG') == '1':
+    with open('api_test.log', 'a') as file:
+        file.write(output)
+
+# requête 4
+r = requests.get(
+    url='http://{address}:{port}/v2/sentiment'.format(address=api_address, port=api_port),
+    params= {
+        'username': 'alice',
+        'password': 'wonderland',
+        'sentence': 'that sucks'
+    }
+)
+
+output = '''
+============================
+    Content test
+============================
+
+request done at "/v2/sentiment"
+| username="alice"
+| password="wonderland"
+| sentence="that sucks"
 
 Expected result < 0; 
 actual result = {score}
